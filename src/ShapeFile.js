045034272a2f81f2c7654f77d21f5b3652ada72a
mnanwarmz <mnanwarmz@gmail.com>
2020-04-13 17:10:27 +0800

    createLeafletElement()

--- src/ShapeFile.js
+++ src/ShapeFile.js
@@ -1,23 +1,22 @@
-import React, {PropTypes}  from 'react';
-import {Path} from 'react-leaflet';
-import Shapefile from './leaflet.shpfile';
-
+import React, { PropTypes } from "react";
+import { Path } from "react-leaflet";
+import Shapefile from "./leaflet.shpfile";
 
 export default class ShapeFile extends Path {
   static propTypes = {
-    data: PropTypes.object.isRequired
+    data: PropTypes.object.isRequired,
   };
 
   componentWillMount() {
+    ShapeFile.prototype.createLeafletElement = function createLeafletElement() {};
     super.componentWillMount();
-    const { data, map: _map, layerContainer: _lc, ...props, } = this.props;
-    this.leafletElement = L.shapefile(data, props); 
+    const { data, map: _map, layerContainer: _lc, ...props } = this.props;
+    this.leafletElement = L.shapefile(data, props);
   }
- 
-   componentDidUpdate(prevProps) {
-   //todo
-    
-     this.setStyleIfChanged(prevProps, this.props);
+
+  componentDidUpdate(prevProps) {
+    //todo
+
+    this.setStyleIfChanged(prevProps, this.props);
   }
 }
-
